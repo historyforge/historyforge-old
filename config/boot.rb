245cdcb5a6d85c021837dd32da31d402a3612001
David Furber <furberd@gmail.com>
2015-09-08 22:48:44 -0400

    add ruby mapscript

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+$: << ENV['RUBY_MAPSCRIPT_PATH']
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
