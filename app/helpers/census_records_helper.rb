a6756a63f059514342f57f86ead553f6253c8354
David Furber <furberd@gmail.com>
2017-01-14 13:09:45 -0500

    better blank handling on 1910 census record form

--- app/helpers/census_records_helper.rb
+++ app/helpers/census_records_helper.rb
@@ -5,21 +5,21 @@ module CensusRecordsHelper
   end
 
   def yes_no_choices
-    [["blank", 'nil'], ["yes", true], ["no", false]]
+    [["blank", nil], ["yes", true], ["no", false]]
   end
 
   def prepare_blanks_for_1910_census(record)
-    record.civil_war_vet ||= 'nil'
-    record.unemployed ||= 'nil'
-    record.naturalized_alien ||= 'nil'
-    record.employment ||= 'nil'
-    record.owned_or_rented ||= 'nil'
-    record.mortgage ||= 'nil'
-    record.farm_or_house ||= 'nil'
-    record.civil_war_vet ||= 'nil'
-    record.can_read ||= 'nil'
-    record.can_write ||= 'nil'
-    record.attended_school ||= 'nil'
+    record.civil_war_vet     = 'nil' if record.civil_war_vet.nil?
+    # record.unemployed        = 'nil' if record.unemployed.nil?
+    record.naturalized_alien = 'nil' if record.naturalized_alien.nil?
+    record.owned_or_rented   = 'nil' if record.owned_or_rented.nil?
+    record.mortgage          = 'nil' if record.mortgage.nil?
+    record.farm_or_house     = 'nil' if record.farm_or_house.nil?
+    record.civil_war_vet     = 'nil' if record.civil_war_vet.nil?
+    # record.can_read          = 'nil' if record.can_read.nil?
+    # record.can_write         = 'nil' if record.can_write.nil?
+    # record.attended_school   = 'nil' if record.attended_school.nil?
+    # raise record.inspect
   end
 
 end
