2608e02e53e6687d00ac69866eed778290f7dbb1
David Furber <furberd@gmail.com>
2017-04-05 08:16:18 -0400

    migration to add two checkboxes to census 1910 records

---
+++ db/migrate/20170115034657_add_some_bools_to_census1910.rb
@@ -0,0 +1,9 @@
+class AddSomeBoolsToCensus1910 < ActiveRecord::Migration
+  def change
+    add_column :census_1910_records, :provisional, :boolean, default: false
+    add_column :census_1910_records, :foreign_born, :boolean, default: false
+    Census1910Record.update_all provisional: false, foreign_born: false
+    Census1910Record.where(profession: nil).update_all profession: 'None'
+    Census1910Record.where(profession: '').update_all profession: 'None'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115023907) do
+ActiveRecord::Schema.define(version: 20170115034657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,8 @@ ActiveRecord::Schema.define(version: 20170115023907) do
     t.boolean  "deaf_dumb",                        default: false
     t.text     "notes"
     t.string   "civil_war_vet",         limit: 2
+    t.boolean  "provisional",                      default: false
+    t.boolean  "foreign_born",                     default: false
   end
 
   add_index "census_1910_records", ["building_id"], name: "index_census_1910_records_on_building_id", using: :btree
