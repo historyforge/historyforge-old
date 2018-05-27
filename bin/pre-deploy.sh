0abfa2b32f8fac67b3d246453fca3053f48368d9
David Furber <furberd@gmail.com>
2018-05-27 15:50:15 -0400

    try another angle

--- bin/pre-deploy.sh
+++ bin/pre-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "--> Installing geo packages"
+apt-get update -qq && apt-get install -y \
+    build-essential libxml2-dev libxslt-dev \
+    libmapserver2 gdal-bin libgdal-dev ruby-mapscript  \
+     && rm -rf /var/lib/apt/lists/*
+
 echo "--> Compiling assets"
 bundle exec rails assets:precompile
 
