e03254c4b7f1756dc0543753e873e34b1e647efd
David Furber <furberd@gmail.com>
2019-08-29 17:27:36 -0400

    more of that

--- app/controllers/wms/layer_controller.rb
+++ app/controllers/wms/layer_controller.rb
@@ -10,17 +10,15 @@ class Wms::LayerController < Wms::BaseController
     raster.tileitem = "Location"
 
     raster.status = Mapscript::MS_ON
-    #raster.setProjection( "+init=" + str(epsg).lower() )
     raster.dump = Mapscript::MS_TRUE
 
-    #raster.setProjection('init=epsg:4326')
     raster.metadata.set('wcs_formats', 'GEOTIFF')
     raster.metadata.set('wms_title', @layer.name)
     raster.metadata.set('wms_srs', 'EPSG:4326 EPSG:3857 EPSG:4269 EPSG:900913')
     raster.debug = Mapscript::MS_TRUE
     send_map_data(map, ows)
-  # rescue RuntimeError => e
-  #   [nil, nil]
+  rescue RuntimeError => e
+    [nil, nil]
   end
 
 
