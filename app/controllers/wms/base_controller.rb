da0267d80560b4d11996670ddc81971a842ea0a0
David Furber <furberd@gmail.com>
2019-08-29 17:13:40 -0400

    hello again

--- app/controllers/wms/base_controller.rb
+++ app/controllers/wms/base_controller.rb
@@ -87,10 +87,9 @@ class Wms::BaseController < ActionController::Base
     map.OWSDispatch(ows)
     content_type = Mapscript.msIO_stripStdoutBufferContentType || "text/plain"
     result_data = Mapscript.msIO_getStdoutBufferBytes
-    # send_data result_data, type: content_type, disposition: "inline"
     [content_type, result_data]
-  # rescue Mapscript::MapserverError
-  #   [nil, nil]
+  rescue Mapscript::MapserverError
+    [nil, nil]
   ensure
     Mapscript.msIO_resetHandlers
   end
