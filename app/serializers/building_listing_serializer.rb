15692fbcba7d4af5a53e7be1a52a4f1e146bc0e8
David Furber <furberd@gmail.com>
2019-09-30 15:18:48 -0400

    cache experiment

--- app/serializers/building_listing_serializer.rb
+++ app/serializers/building_listing_serializer.rb
@@ -1,16 +1,18 @@
 class BuildingListingSerializer
   include FastJsonapi::ObjectSerializer
 
+  cache_options enabled: true
+
   attributes :id, :name, :year_earliest, :year_latest,
              :street_address, :city, :state, :postal_code, :building_type_id,
              :latitude, :longitude
 
   # has_many :architects, serializer: ArchitectSerializer
 
-  attribute :residents do |object|
-    object.residents.present? && object
-        .residents
-        .map { |item| CensusRecordSerializer.new(item).as_json }
-        .group_by { |item| item['data']['attributes']['year'] }
-  end
+  # attribute :residents do |object|
+  #   object.residents.present? && object
+  #       .residents
+  #       .map { |item| CensusRecordSerializer.new(item).as_json }
+  #       .group_by { |item| item['data']['attributes']['year'] }
+  # end
 end
