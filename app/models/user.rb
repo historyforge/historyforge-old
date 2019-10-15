cff44e12442ef49babdc5274c3c43477addb9257
David Furber <furberd@gmail.com>
2019-10-15 13:52:23 -0400

    remove mapwarper stuff from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,6 @@ class User < ApplicationRecord
   has_many :permissions
   has_many :roles, :through => :permissions
 
-  has_many :my_maps, :dependent => :destroy
-  has_many :maps, -> { distinct }, :through => :my_maps
-
-  has_many :layers, :dependent => :destroy
-
   has_many :census1900_records, dependent: :nullify, foreign_key: :created_by_id
   has_many :census1910_records, dependent: :nullify, foreign_key: :created_by_id
   has_many :census1920_records, dependent: :nullify, foreign_key: :created_by_id
@@ -21,8 +16,6 @@ class User < ApplicationRecord
   validates_length_of      :login,    :within => 3..40
   validates_uniqueness_of  :login, :scope => :email, :case_sensitive => false
 
-  after_destroy :delete_maps
-
   def name
     login
   end
@@ -31,34 +24,9 @@ class User < ApplicationRecord
     self.roles.find_by_name(name) ? true : false
   end
 
-  def own_maps
-    Map.where(["owner_id = ?", self.id])
-  end
-
-  def own_this_map?(map_id)
-    Map.exists?(:id => map_id.to_i, :owner_id => self.id)
-  end
-
-  def own_this_layer?(layer_id)
-    Layer.exists?(:id => layer_id.to_i, :user_id => self.id)
-  end
-
   #Called by Devise
   #Method checks to see if the user is enabled (it will therefore not allow a user who is disabled to log in)
   def active_for_authentication?
     super and self.enabled?
   end
-
-  protected
-
-  #called after the user has been destroyed
-  #delete all user maps
-  def delete_maps
-    own_maps.each do | map |
-      logger.debug "deleting map #{map.inspect}"
-      map.destroy
-    end
-  end
-
-
 end
