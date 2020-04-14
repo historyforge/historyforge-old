d20605a526eff4202a9eb722cc8c50a1fa7d4e81
David Furber <furberd@gmail.com>
2020-04-14 09:58:22 -0400

    add industry to 1900 fields

--- app/services/census_record1900_search.rb
+++ app/services/census_record1900_search.rb
@@ -7,7 +7,7 @@ class CensusRecord1900Search < CensusRecordSearch
     %w[page_number page_side line_number county city ward enum_dist street_address dwelling_number family_id
       name relation_to_head sex race age birth_year birth_month marital_status years_married
       num_children_born num_children_alive pob pob_father pob_mother foreign_born year_immigrated years_in_us
-      naturalized_alien language_spoken profession
+      naturalized_alien language_spoken profession industry
       unemployed_months can_read can_write attended_school
       owned_or_rented mortgage farm_or_house
       notes latitude longitude
