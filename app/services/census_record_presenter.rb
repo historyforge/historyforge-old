4c2834731cf0539dab580e9bbc1e3669823bb90a
David Furber <furberd@gmail.com>
2020-09-14 10:09:14 -0400

    census record race null check

--- app/services/census_record_presenter.rb
+++ app/services/census_record_presenter.rb
@@ -29,7 +29,7 @@ class CensusRecordPresenter < Struct.new(:model, :user)
   end
 
   def race
-    I18n.t("census_codes.race.#{model.race.downcase}", default: model.race)
+    model.race && I18n.t("census_codes.race.#{model.race.downcase}", default: model.race) || 'left blank'
   end
 
   def locality
