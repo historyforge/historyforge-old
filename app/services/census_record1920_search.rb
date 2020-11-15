f038a9b78230d4f7c3651c11c743d9862af902f5
David Furber <furberd@gmail.com>
2020-11-15 17:11:48 -0500

    reorder some fields

--- app/services/census_record1920_search.rb
+++ app/services/census_record1920_search.rb
@@ -5,13 +5,13 @@ class CensusRecord1920Search < CensusRecordSearch
 
   def all_fields
     %w[page_number page_side line_number county city ward enum_dist street_address locality dwelling_number family_id
-      name relation_to_head sex race age marital_status
-      pob pob_father pob_mother
-      mother_tongue mother_tongue_father mother_tongue_mother
-      foreign_born year_immigrated year_naturalized
-      naturalized_alien
+      name relation_to_head owned_or_rented mortgage sex race age marital_status
+      foreign_born year_immigrated naturalized_alien year_naturalized
+      attended_school can_read can_write
+      pob mother_tongue pob_father mother_tongue_father pob_mother mother_tongue_mother
+      can_speak_english
       profession industry employment employment_code
-      owned_or_rented mortgage farm_or_house
+      farm_or_house farm_schedule
       notes latitude longitude
     ]
   end
