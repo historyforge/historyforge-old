e065d61f0be479c79e75acc7b7ee83d63e28c1ca
David Furber <dfurber@gorges.us>
2020-12-06 20:21:48 -0500

    schema after migration ran

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_212348) do
+ActiveRecord::Schema.define(version: 2020_12_07_010511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
