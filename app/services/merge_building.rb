aa86ae44f24406968bb0d531f58ea2170db886eb
David Furber <dfurber@gorges.us>
2020-12-06 19:49:25 -0500

    be more final with building merges

--- app/services/merge_building.rb
+++ app/services/merge_building.rb
@@ -4,11 +4,23 @@ class MergeBuilding
   end
 
   def perform
+    merge_building_data
     merge_census_records
     merge_photographs
+    @target.save!
+    @source.destroy!
   end
 
   private
+
+  def merge_building_data
+    @source.attributes.keys.each do |attr|
+      next if attr =~ /created|updated|reviewed|address/
+
+      create_or_append(attr)
+    end
+    create_or_append(:architect_ids)
+  end
 
   def merge_census_records
     @source.with_residents.residents.each do |record|
@@ -20,6 +32,20 @@ class MergeBuilding
   def merge_photographs
     @source.photos.each do |photo|
       photo.buildings << @target
+    end
+  end
+
+  def create_or_append(field)
+    source_val = @source.send(field)
+    target_val = @target.send(field)
+    return if source_val.blank? || source_val == target_val
+
+    if target_val.blank?
+      @target.send("#{field}=", source_val)
+    elsif source_val.is_a?(String)
+      @target.send("#{field}=", [target_val, source_val].join("\r\n"))
+    elsif source_val.is_a?(Array)
+      @target.send("#{field}=", target_val.concat(source_val).uniq)
     end
   end
 end
