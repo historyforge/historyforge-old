435dec5ca2adeb567609a9faf05797c1692d7199
David Furber <dfurber@gorges.us>
2020-12-19 11:20:36 -0500

    fix buildings

---
+++ db/migrate/20201219161442_fix_building_descriptions.rb
@@ -0,0 +1,21 @@
+class FixBuildingDescriptions < ActiveRecord::Migration[6.0]
+  def change
+    Building.find_each do |row|
+      next if row.description =~ /\W/ || row.annotations =~ /\W/
+
+      fix_it(row, row.paper_trail.previous_version)
+    end
+  end
+
+  def fix_it(row, version)
+    return if version.blank?
+
+    if version.description =~ /\W/ || version.annotations =~ /\W/
+      row.description = version.description
+      row.annotations = version.annotations
+      row.save
+    else
+      fix_it(row, version.paper_trail.previous_version)
+    end
+  end
+end
